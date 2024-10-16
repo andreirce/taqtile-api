e0913892dd3faecac31f9162c9c2689b56b133bc
Andrei Levi <andreilevi21@gmail.com>
2024-10-16 04:00:09 -0300

    feat: establish connection with testdb for testing

--- test/index.ts
+++ test/index.ts
@@ -1,8 +1,11 @@
 import { before, after } from 'mocha';
 import { bootstrap } from '../src/server';
 import { ApolloServer } from '@apollo/server';
+import dotenv from 'dotenv';
 
 let server: ApolloServer;
+
+dotenv.config({ path: '../.env.test' });
 
 before(async () => {
   server = await bootstrap(4001);
