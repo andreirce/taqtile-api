832845b469bbb6b61f6a90e7e50df09081607db5
Andrei Levi <andreilevi21@gmail.com>
2024-10-16 04:05:44 -0300

    test: improve clarity of test messages

--- test/hello-world.test.ts
+++ test/hello-world.test.ts
@@ -2,8 +2,8 @@ import { describe, it } from 'mocha';
 import { expect } from 'chai';
 import axios from 'axios';
 
-describe('Hello World suite', () => {
-  it('Test if Hello World query returns a "Hello World string"', async () => {
+describe('Hello World testing', () => {
+  it('It should return the message hello world', async () => {
     const response = await axios.post('http://localhost:4001/graphql', {
       query: `
         query HelloWorld {
