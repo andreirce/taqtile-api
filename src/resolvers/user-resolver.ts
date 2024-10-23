32e65f911e736a67e98357c74250083c91d4cd20
Andrei Levi <andreilevi21@gmail.com>
2024-10-23 20:20:46 -0300

    feat: add login function to service and login user mutation

--- src/resolvers/user-resolver.ts
+++ src/resolvers/user-resolver.ts
@@ -2,6 +2,8 @@ import { Arg, Mutation, Query, Resolver } from 'type-graphql';
 import { UserModel } from '../models/user-model';
 import { UserInput } from '../inputs/user-input';
 import { UserService } from '../services/user-service';
+import { LoginModel } from '../models/login-model';
+import { LoginInput } from '../inputs/login-input';
 
 @Resolver()
 export class UserResolver {
@@ -13,5 +15,10 @@ export class UserResolver {
   @Mutation(() => UserModel)
   async createUser(@Arg('data', () => UserInput) data: UserInput) {
     return UserService.createUser(data);
+  }
+
+  @Mutation(() => LoginModel)
+  async login(@Arg('data', () => LoginInput) data: LoginInput) {
+    return UserService.loginUser(data);
   }
 }
