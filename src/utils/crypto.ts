079656946367a381c88f721c943cb6b8dbbdd79c
Andrei Levi <andreilevi21@gmail.com>
2024-10-24 21:10:24 -0300

    refactor: simplify return of hashPassword and comparePassword functions

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -3,11 +3,9 @@ import { hash, compare } from 'bcrypt';
 const saltRounds = 10;
 
 export async function hashPassword(password: string): Promise<string> {
-  const hashedPassword = await hash(password, saltRounds);
-  return hashedPassword;
+  return await hash(password, saltRounds);
 }
 
 export async function comparePassword(password: string, hashedPassword: string): Promise<boolean> {
-  const comparePassword = await compare(password, hashedPassword);
-  return comparePassword;
+  return await compare(password, hashedPassword);
 }
