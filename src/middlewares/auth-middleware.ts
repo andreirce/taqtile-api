dd67fa534659ef955a0132171a3251e301145b37
Andrei Levi <andreilevi21@gmail.com>
2024-10-30 02:02:27 -0300

    feat: add CustomJwtPayload interface to enhance JWT type safety

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -2,8 +2,8 @@ import jwt from 'jsonwebtoken';
 
 import { MiddlewareFn, createMethodMiddlewareDecorator } from 'type-graphql';
 import { UnauthorizedException } from '../exceptions/unauthorized-exception';
-import { User } from '../interfaces/user-interface';
 import { ExpressContext } from '../interfaces/express-context-interface';
+import { CustomJwtPayload } from '../interfaces/custom-jwt-payload-interface';
 
 const authHandler: MiddlewareFn<ExpressContext> = ({ context }, next) => {
   const authorization = context.req.headers?.authorization;
@@ -14,9 +14,9 @@ const authHandler: MiddlewareFn<ExpressContext> = ({ context }, next) => {
 
   try {
     const token = authorization.split(' ')[1];
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET) as CustomJwtPayload;
 
-    context.req.user = decoded as User;
+    context.req.user = decoded;
   } catch {
     throw new UnauthorizedException('Acesso negado! Você não tem permissão para acessar este recurso.');
   }
