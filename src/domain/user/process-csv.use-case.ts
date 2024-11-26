c9b0618f24c92a395f858c84c4de1dc7f3f03a71
Andrei Levi <andreilevi21@gmail.com>
2024-11-26 16:04:58 -0300

    refactor: remove console.log and fixing error name

--- src/domain/user/process-csv.use-case.ts
+++ src/domain/user/process-csv.use-case.ts
@@ -1,18 +1,18 @@
 import { CsvService } from '@core/upload-files/csv.service';
 import { Service } from 'typedi';
 import { FileUpload } from 'graphql-upload-ts';
-import { invalidFileError } from '@core/error/invalid-file-upload.error';
+import { InvalidFileError } from '@core/error/invalid-file-upload.error';
 
 @Service()
 export class ProcessCsvUseCase {
-  constructor(private readonly csvService: CsvService) {}
+    constructor(private readonly csvService: CsvService) { }
 
-  async exec(file: FileUpload) {
-    if (file.mimetype !== 'text/csv') {
-      throw new invalidFileError('Somente arquivos CSV são permitidos!');
+    async exec(file: FileUpload) {
+        if (file.mimetype !== 'text/csv') {
+            throw new InvalidFileError('Somente arquivos CSV são permitidos!');
+        }
+
+        const readStream = file.createReadStream();
+        return this.csvService.parseCsv(readStream);
     }
-
-    const readStream = file.createReadStream();
-    return this.csvService.parseCsv(readStream);
-  }
 }
