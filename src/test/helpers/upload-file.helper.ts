85e5b6ac2f82216e7576039758eb416dfc3c9345
Andrei Levi <andreilevi21@gmail.com>
2024-11-26 14:55:32 -0300

    test: add file upload mutation test for valid and invalid CSV uploads

---
+++ src/test/helpers/upload-file.helper.ts
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import { ReadStream } from 'node:fs';
+import FormData from 'form-data';
+
+export async function uploadFileForTest(file: ReadStream, token: string | null) {
+  const uploadFileMutation = `
+    mutation UploadCsv($file: Upload!) {
+      uploadCsv(file: $file)
+    }
+  `;
+
+  const tokenAuthorization = token === null ? token : `bearer ${token}`;
+
+  const formData = new FormData();
+  formData.append(
+    'operations',
+    JSON.stringify({
+      query: uploadFileMutation,
+      variables: { file: null },
+    }),
+  );
+
+  formData.append(
+    'map',
+    JSON.stringify({
+      0: ['variables.file'],
+    }),
+  );
+
+  formData.append('0', file);
+
+  const headers = {
+    ...formData.getHeaders(),
+    Authorization: tokenAuthorization,
+  };
+
+  const response = await axios.post('http://localhost:4001/graphql', formData, { headers });
+
+  console.log(response.data);
+  return { data: response.data.data?.uploadCsv, errors: response.data.errors };
+}
