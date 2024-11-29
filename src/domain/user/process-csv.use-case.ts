f41a8a60c872fdcf2fedfb89450defaa6ddce9b3
Andrei Levi <andreilevi21@gmail.com>
2024-11-29 09:23:30 -0300

    feat: add users creation from CSV file logic with field validation

--- src/domain/user/process-csv.use-case.ts
+++ src/domain/user/process-csv.use-case.ts
@@ -2,17 +2,50 @@ import { CsvService } from '@core/upload-files/csv.service';
 import { Service } from 'typedi';
 import { FileUpload } from 'graphql-upload-ts';
 import { InvalidFileError } from '@core/error/invalid-file-upload.error';
+import { UserInputModel } from '@domain/model';
+import { InvalidInputCsvError } from '@core/error/invalid-input-csv.error';
+import { hashPassword } from '@core/security/crypto/crypto';
+import { UserDbDataSource } from '@data/user/user.db.datasource';
+import { UserAlreadyExistsError } from '@core/error';
 
 @Service()
 export class ProcessCsvUseCase {
-    constructor(private readonly csvService: CsvService) { }
+  constructor(
+    private readonly csvService: CsvService,
+    private readonly datasource: UserDbDataSource,
+  ) {}
 
-    async exec(file: FileUpload) {
-        if (file.mimetype !== 'text/csv') {
-            throw new InvalidFileError('Somente arquivos CSV são permitidos!');
-        }
-
-        const readStream = file.createReadStream();
-        return this.csvService.parseCsv(readStream);
+  async exec(file: FileUpload) {
+    if (file.mimetype !== 'text/csv') {
+      throw new InvalidFileError('Somente arquivos CSV são permitidos!');
     }
+
+    const readStream = file.createReadStream();
+    const csvData = (await this.csvService.parseCsv(readStream)) as UserInputModel[];
+
+    const usersToCreate = await Promise.all(csvData.map((row) => this.processUserData(row)));
+
+    await this.datasource.createMany(usersToCreate);
+  }
+
+  private async processUserData(row: UserInputModel) {
+    const { name, email, password, bitrhDate } = row;
+
+    if (!name || !email || !password) {
+      throw new InvalidInputCsvError(`Dados inválidos no CSV: nome, email e senha são obrigatórios.`);
+    }
+
+    const existingUser = await this.datasource.findByEmail(email);
+
+    if (existingUser) {
+      throw new UserAlreadyExistsError(`O usuário ${email} ja está registrado!`);
+    }
+
+    return {
+      name,
+      email,
+      password: await hashPassword(password),
+      birthDate: bitrhDate ? new Date(bitrhDate) : null,
+    };
+  }
 }
